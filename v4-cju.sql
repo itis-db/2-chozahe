309aaa3ff0822882ddd6b6a36f49f9458e52101f
chozahe <144026241+chozahe@users.noreply.github.com>
2025-02-02 19:44:16 +0300

    Update v4-cju.sql

--- v4-cju.sql
+++ v4-cju.sql
@@ -1,3 +1,35 @@
 /**
-  @author
+  @Трофимов Александр
  */
+
+-- 1
+
+WITH PopularBooks AS (
+    SELECT b.title, COUNT(l.book_id) AS loan_count
+    FROM Books b
+             JOIN Loans l ON b.id = l.book_id
+    GROUP BY b.title
+    ORDER BY loan_count DESC
+    LIMIT 5
+)
+SELECT title, loan_count
+FROM PopularBooks;
+
+-- 2
+
+SELECT r.name AS reader_name, b.title AS book_title, l.loan_date, l.return_date
+FROM Readers r
+         JOIN Loans l ON r.id = l.reader_id
+         JOIN Books b ON l.book_id = b.id
+ORDER BY r.name, l.loan_date;
+
+-- 3
+
+(SELECT a.name AS entity_name, 'Author' AS entity_type
+ FROM Authors a
+ WHERE a.name LIKE 'A%')
+UNION
+(SELECT g.name AS entity_name, 'Genre' AS entity_type
+ FROM Genres g
+ WHERE g.name LIKE 'A%')
+ORDER BY entity_name;
