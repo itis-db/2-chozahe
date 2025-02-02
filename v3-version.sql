3ba1c23666c7a1d303bb66b6d6e37c5a9c8b60e0
chozahe <144026241+chozahe@users.noreply.github.com>
2025-02-02 19:43:49 +0300

    Update v3-version.sql

--- v3-version.sql
+++ v3-version.sql
@@ -1,3 +1,33 @@
 /**
-  @author
+  @Трофимов Александр
  */
+
+ALTER TABLE Books
+    ALTER COLUMN isbn TYPE VARCHAR(13);
+
+ALTER TABLE Books
+    ADD COLUMN description TEXT;
+
+ALTER TABLE Readers
+    ADD COLUMN phone VARCHAR(15);
+
+ALTER TABLE Readers
+    ALTER COLUMN phone SET NOT NULL;
+
+ALTER TABLE Books
+    ADD CONSTRAINT check_year_published CHECK (year_published >= 1800);
+
+ALTER TABLE Readers
+    ADD CONSTRAINT unique_email UNIQUE (email);
+
+--  rollback
+
+ALTER TABLE Readers
+    ALTER COLUMN phone DROP NOT NULL;
+
+ALTER TABLE Books
+    DROP CONSTRAINT check_year_published;
+
+
+ALTER TABLE Readers
+    DROP CONSTRAINT unique_email;
